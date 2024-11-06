493b5d174d9d66ab6b20a09012c8211f9e64c1e0
Othavio Rubim <orubim37@gmail.com>
2024-11-06 08:47:31 -0300

    first commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,12 @@
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user_id' })
+
+const userCanInviteSomeoneElese = ability.can('create', 'Project')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElese)
+console.log(userCanDeleteOtherUsers)
+console.log(userCannotDeleteOtherUsers)
